2d6b3a183e8454e944f990c490a5755bd66e1cce
Rupesh Patel <41280522+datasci201195@users.noreply.github.com>
2018-10-16 19:52:35 +0530

    update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ house <- "./data/household_power_consumption.txt"
 data <- read.table(house, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subset_house <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
+
 #str(subset_house)
 globalActivePower <- as.numeric(subset_house$Global_active_power)
 png("plot1.png", width=480, height=480)
